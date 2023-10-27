99e6c771c0ccc7d730332ea37141276106e0fa69
Garrett <gcamoin5@gmail.com>
2023-10-26 21:00:12 -0400

    created get route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/movies" do
+    movies = Movie.all.order(:title)
+    movies.to_json(include: [:genre])
+  end
+    
   end
 
 end
