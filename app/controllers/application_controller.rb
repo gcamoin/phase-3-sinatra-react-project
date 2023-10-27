b277b9da31b3bd725d3bf4b3e5521ba6aac7e23c
Garrett <gcamoin5@gmail.com>
2023-10-26 21:45:45 -0400

    added POST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,21 @@ class ApplicationController < Sinatra::Base
   get "/movies" do
     movies = Movie.all.order(:title)
     movies.to_json(include: [:genre])
-  end
-    
+
   end
 
+  post "/movies" do
+    movie = Movie.find_or_create_by(title: (params[:movie] [:title]))
+    genre = Genre.find_or_create_by(name: params[:genre] [:name])
+    genre.update(
+      name: params[:genre] [:name],
+    ) 
+    movie = Movie.create(
+      title: params[:title],
+      genre_id: genre.id,
+      year: params[year],
+    )
+  end
+
+end
 end
