0e79a45b1f5c2c3975d5c6c1f857b5a0b90efb11
Garrett <gcamoin5@gmail.com>
2023-11-01 22:58:41 -0400

    added README

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,37 @@ class ApplicationController < Sinatra::Base
   # Add your routes here
   get "/movies" do
     movies = Movie.all.order(:title)
-    movies.to_json(include: [:genre])
+    movies.to_json
 
   end
 
   get "/genres" do 
     genres = Genre.all.order(:name)
-    genres.to_json
+    genres.to_json(include: :movies)
   end
 
   delete '/movies/:id' do
     movie = Movie.find(params[:id])
     movie.destroy
+    movie.to_json
+  end
+
+  post '/genres' do
+    genre = Genre.create(
+      name: params[:name]
+    )
+    genre.to_json
+  end
+
+  post '/movies' do
+    
+    movie = Movie.create(
+      title: params[:title],
+      year: params[:year],
+      genre_id: params[:genre_id],
+      likes: params[:likes],
+      dislikes: params[:dislikes]
+    )
     movie.to_json
   end
 
